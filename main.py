1cff5737bcbc578c2b739ae1e5c3164911be40af
Mateusz Nawrocki <nawrockim95@gmail.com>
2024-09-03 11:57:26 +0200

    add folder with screenshot

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ list1 = ['chleb', 'bułki', 'pączek']
 list2 = ['marchew', 'seler', 'rukola']
 dict1 = {'piekarnia': list1, 'warzywniak': list2}
 print("Lista zakupów")
+
+for i , v in dict1.items():
+    print("Idę do", i.capitalize(), "i kupuję", v)
+print("W sumie kupuję", (list1.__len__()+list2.__len__()), "produktów")
