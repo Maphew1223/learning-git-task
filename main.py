6a18b929ee2bac41d99b084891abbe91a932ae19
Mateusz Nawrocki <nawrockim95@gmail.com>
2024-09-05 00:25:59 +0200

    add loop lesson

--- main.py
+++ main.py
@@ -6,3 +6,20 @@ print("Lista zakupów")
 for i , v in dict1.items():
     print("Idę do", i.capitalize(), "i kupuję", v)
 print("W sumie kupuję", (list1.__len__()+list2.__len__()), "produktów")
+
+
+for x in range(101):
+    if x % 3 == 0:
+        print(x)
+print()
+
+j = 0
+while True:
+    if j % 3 == 0:
+        print(j)
+    else:
+        pass
+    j += 1
+    if j >100:
+        
+        break
